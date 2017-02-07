3c2576459a27d4fd71bcf3fa95bfe84bc2e4ecf8
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-02-06 23:13:18 -0600

    Adding pre_tasks

--- playbooks/bin/check-jenkins-status.py
+++ playbooks/bin/check-jenkins-status.py
@@ -26,6 +26,16 @@ def main(args):
         sys.exit(3)
 
     build_result = res_json.get('result', '')
+    build_timestamp = res_json.get('timestamp', 0)
+    commit_items = res_json.get('changeSet', {}).get('items', [{}])
+    commit_timestamps = [x['timestamp'] for x in commit_items]
+    commit_timestamp = max(commit_timestamps)
+    if len(str(build_timestamp)):
+        build_timestamp = build_timestamp / 1000
+    if len(str(commit_timestamp)):
+        commit_timestamp = commit_timestamp / 1000
+    delta = commit_timestamp - build_timestamp
+
     if build_result != 'SUCCESS':
         print "Latest build was not succesfull."
         sys.exit(4)
